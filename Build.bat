5d8d7b9a84bcd614c4a0867a73eddb81f10cba37
Trent Patterson <jtrent238@outlook.com>
2016-05-16 19:20:06 -0400

    Add files via upload

---
+++ Build.bat
@@ -0,0 +1,5 @@
+@echo off
+jar cf Calculator.jar C:\Users\trent\Desktop\Calculator
+echo Build Complete Or Failed! (If This Is First Line Then It Was Successful!!!)
+pause
+close
---
+++ Compile.bat
@@ -0,0 +1,5 @@
+@echo off
+javac Calculator.java
+echo Compile Complete Or Failed! (If This Is First Line Then It Was Successful!!!)
+pause
+close
